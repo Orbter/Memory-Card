1df789cd545366b419a755c0309a328a5bdc56a1
Orbter <assouline39@gmail.com>
2024-09-23 19:43:25 +0300

    made the animation work

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useRef } from 'react';
+
 import '../../style/card.css';
 import luffyTest from '../../assets/luffy-test.png';
+import VanillaTilt from 'vanilla-tilt';
+
 function Card() {
+  const cardRef = useRef(null);
+  useEffect(() => {
+    VanillaTilt.init(cardRef.current);
+    return () => cardRef.current.vanillaTilt.destroy();
+  }, []);
+
   return (
-    <div className='card'>
+    <div className='card' ref={cardRef}>
       <div className='card-image'>
         <img src={luffyTest} className='photo-card'></img>
       </div>
