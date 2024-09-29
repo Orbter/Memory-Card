f254b1b27a59646d13d9d3281c726f5b0ac228d4
Orbter <assouline39@gmail.com>
2024-09-29 03:10:15 +0300

    made an update on the start game

--- src/logic/startGameEasy.jsx
+++ src/logic/startGameEasy.jsx
@@ -1,6 +1,22 @@
+import { useContext } from 'react';
+
 import { choosingCharacters } from './choosingCharacters';
 import { attachPhotos } from '../api/gettingPhotos';
-function startGameEasy(onePieceCharacters) {
+import { GameContext } from '../GameProvider';
+function startGameEasy() {
+  const { gameMemory } = useContext(GameContext);
   const charactersChosen = choosingCharacters(6);
-  const listOfChosenCharacters = 
+  const temporaryList = [];
+  const {
+    listOfCharacterActive,
+    setListOfCharacterActive,
+    onePieceCharacters,
+    setOnePieceCharacters,
+  } = gameMemory;
+
+  for (let index = 0; index < 6; index++) {
+    const character = onePieceCharacters[charactersChosen[index]];
+    temporaryList.push(character);
+  }
+  setListOfCharacterActive(temporaryList);
 }
